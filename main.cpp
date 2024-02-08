8688481d6101e5f1453c97704d0c855e9365aa4f
ziogit <r.morpanini@gmail.com>
2024-02-08 21:16:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::cout << "Let's start";
     std::cout << "Don't know why I'm doing this";
-    std::cout << "I need some";
+    std::cout << "I need some more";
     std::cout << "At least 30 commits";
     return 0;
 }
