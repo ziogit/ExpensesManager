0b572cf5a6dbd4f4b84fefabdaf553e0e73f20f3
ziogit <r.morpanini@gmail.com>
2024-02-09 19:06:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,16 @@ int main() {
     std::cout << "Don't know why I'm doing this";
     std::cout << "I need some more";
     std::cout << "At least 30 commits";
+
+    enum Color { red, white, green };
+    Color r = red;
+ 
+    switch(r)
+    {
+        case red  : std::cout << "red\n";   break;
+        case white: std::cout << "white\n"; break;
+        case green : std::cout << "green\n";  break;
+    }
+    
     return 0;
 }
