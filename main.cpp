aefed8ea58606ad806330a56ef14f97061162115
ziogit <r.morpanini@gmail.com>
2024-02-17 10:39:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 int main() {
     std::cout << "Let's start";
     std::cout << "Don't know why I'm doing this";
-    std::cout << "I need some more";
-    std::cout << "At least 30 commits";
-    std::cout << "Slowly but walking";
 
     enum Color { red, white, green, orange };
     Color r = red;
