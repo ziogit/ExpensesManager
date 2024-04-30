3738c6226c23c8acd3fd58a5bffb2f30ef13b346
ziogit <r.morpanini@gmail.com>
2024-04-30 19:36:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
         case white: std::cout << "white\n"; break;
         case green : std::cout << "green\n";  break;
         case yellow : std::cout << "yellow\n";  break;
+        case blue : std::cout << "blue\n";  break;
         default: std::cout << "null\n";
     }
     
