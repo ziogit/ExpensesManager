41227fa12e6d4769816eefd00b189d61fb31cab7
ziogit <r.morpanini@gmail.com>
2024-06-08 09:55:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::cout << "I'm tired and sick, but I'm building.";
 
-    enum Color { red, white, green, yellow, blue, white, transparent };
+    enum Color { red, white, green, yellow, blue, white, transparent, none };
     Color c = 0xFFFFFF;
  
     switch (c)
